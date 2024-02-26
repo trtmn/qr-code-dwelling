03d18a01e577c2ac31c3af07b0581d81260253f9
Matt Troutman <git@trtmn.com>
2024-02-26 16:14:38 -0600

    updates to app & template

--- my_app.py
+++ my_app.py
@@ -1,6 +1,4 @@
-import urllib.parse
-
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from main import generate_qr_code, resize_qr_code, apply_logo, generate_and_save_qr_code
 
 app = Flask(__name__)
@@ -20,34 +18,7 @@ def index():
         qr_code_stream = generate_and_save_qr_code(data, logo)
         return send_file(qr_code_stream, mimetype='image/png')
 
-    return '''
-<!doctype html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script>
-        function submitForm(event) {
-            event.preventDefault();
-            var data = document.getElementById('data').value;
-            var logo = document.getElementById('logo').value;
-            var url = '/' + '?data=' + encodeURIComponent(data);
-            if (logo) {
-                url += '&logo=' + encodeURIComponent(logo);
-            }
-            window.location.href = url;
-        }
-    </script>
-</head>
-<body>
-    <title>QR Code Generator</title>
-    <form onsubmit="submitForm(event)">
-        URL to QR-Codify: <input type="text" id="data" name="data" value="https://thedwelling.church"><br>
-        Logo: <input type="text" id="logo" name="logo" placeholder="Leave Blank for dwelling logo"><br>
-        <input type="submit" value="Generate">
-    </form>
-</body>
-</html>
-'''
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
