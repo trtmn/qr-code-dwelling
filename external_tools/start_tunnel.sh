4a677fd4b1caedeb5dd68c33e975f754a34a87a9
Matt Troutman <git@trtmn.com>
2024-03-06 12:33:17 -0600

    no message

--- external_tools/start_tunnel.sh
+++ external_tools/start_tunnel.sh
@@ -1,1 +1,3 @@
+# Check if the cloudflared service is already installed.
+# If not, install it using the provided tunnel key.
 if [ ! -f /etc/init.d/cloudflared ]; then cloudflared service install {{ op://Automation Secrets/QR_Maker/tunnel_key }}; fi
--- spinup.sh
+++ spinup.sh
@@ -1,9 +1,11 @@
 # This script is used to set up a cloudflared service and clean up supervisord logs and pid files.
 
-# Check if the cloudflared service is already installed.
-# If not, install it using the provided tunnel key.
+
 op inject --force -i ./external_tools/start_tunnel.sh -o ./external_tools/start_tunnel.sh
 #if [ ! -f /etc/init.d/cloudflared ]; then cloudflared service install $tunnel_key; fi
+
+# Make the shell scripts executable.
+chmod u+x ./external_tools/*.sh
 
 # Remove the supervisord log file if it exists.
 rm ./supervisord.log
@@ -14,12 +16,8 @@ rm ./supervisord.pid
 # Remove the app.log file if it exists
 rm ./app.log
 
-
 #generate the nginx configuration file from the template using 1password CLI
 op inject -i ./nginx.conf.template -o /etc/nginx/conf.d/nginx.conf
-
-## Apply the nginx configuration.
-#envsubst '$SERVER_NAME' < ./nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 
 # Start the supervisord service.
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
