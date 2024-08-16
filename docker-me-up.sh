8999bcc73eee32d21a0bc39008fbbb0dc0a50121
Matt Troutman <git@trtmn.com>
2024-08-16 13:34:36 -0500

    Add some comments on running the thing.

--- docker-me-up.sh
+++ docker-me-up.sh
@@ -1,1 +1,9 @@
+# A simple script to start the docker containers
+
+# PREREQUISITES:
+# 1. Docker
+# 2. Docker Compose (official version `docker compose`, not docker-compose)
+# 3. ENV variables set for OP_SERVICE_ACCOUNT_TOKEN)
+# 4 other variables in the service account vault.
+
 docker compose -f ./docker-compose.yml -p qr-maker up --always-recreate-deps --remove-orphans --force-recreate -d --build
