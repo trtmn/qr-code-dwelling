d12dc93a021e4bb5fecf4e63ee648591db988888
Matt Troutman <git@trtmn.com>
2024-08-16 22:28:45 -0500

    updated the up file

--- up.sh
+++ up.sh
@@ -6,4 +6,8 @@
 # 3. ENV variables set for OP_SERVICE_ACCOUNT_TOKEN)
 # 4 other variables in the service account vault.
 
+# Find the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
 docker compose -f ./docker-compose.yml -p qr-maker up --always-recreate-deps --remove-orphans --force-recreate -d --build
